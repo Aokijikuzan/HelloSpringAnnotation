7d8f2b4f25f2140f3868a2f60256a14230415f92
florent <florent@gmail.com>
2018-10-24 16:48:08 +0200

    projet tuto base Spring

---
+++ src/main/java/org/o7planning/spring/MainProgram.java
@@ -0,0 +1,10 @@
+package org.o7planning.spring;
+
+public class MainProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/org/o7planning/spring/lang/impl/English.java
@@ -0,0 +1,18 @@
+package org.o7planning.spring.lang.impl;
+
+import org.o7planning.spring.lang.Language;
+public class English implements Language {
+
+	@Override
+	public String getGreeting() {
+		// TODO Auto-generated method stub
+		return "hello";
+	}
+
+	@Override
+	public String getBye() {
+		// TODO Auto-generated method stub
+		return "bye bye";
+	}
+
+}
